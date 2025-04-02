bec9df8e8ea783a8b85c02a089351cb7305f7930
Piou-piou <pilloud.anthony@gmail.com>
2025-04-02 19:00:59 +0200

    feat: add live 3

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,19 +12,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/list', name: 'index', methods: ['GET'])]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $articles = $em->getRepository(Article::class)->findAll();
+
         return $this->render('pages/article/list.html.twig', [
-            'articles' => [
-                [
-                    'title' => 'Article 1',
-                    'content' => '<strong>Article 1 content</strong>',
-                ],
-                [
-                    'title' => 'Article 2',
-                    'content' => 'Article 2 content',
-                ]
-            ]
+            'articles' => $articles
         ]);
     }
 }
