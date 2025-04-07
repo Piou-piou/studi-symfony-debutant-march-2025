44d4668cf9b54267631a1cacce3991f2f76daf8d
Piou-piou <pilloud.anthony@gmail.com>
2025-04-07 13:00:18 +0200

    feat: add live 4

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,6 +21,53 @@ class ArticleController extends AbstractController
 
         return $this->render('pages/article/list.html.twig', [
             'articles' => $articles
+        ]);
+    }
+
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ArticleType::class);
+        $form->add('submit', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('pages/article/create.html.twig', [
+            'form' => $form->createView(),
+            'form_errors' => $form->getErrors(),
+        ]);
+    }
+
+
+    public function edit(Request $request, EntityManagerInterface $em, Article $article): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->add('submit', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('pages/article/edit.html.twig', [
+            'form' => $form->createView(),
+            'form_errors' => $form->getErrors(),
+            'article' => $article,
         ]);
     }
 }
